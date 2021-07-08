0f38f7f31b1940c7cc1a456b050b205f6ba72a2d
Tori FavWest <practiceandthenpractice@gmail.com>
2021-07-07 20:48:20 -0700

    Add 'runBlocking' launch

--- app/src/main/java/com/favwest/coroutinestoyapp/MainActivity.kt
+++ app/src/main/java/com/favwest/coroutinestoyapp/MainActivity.kt
@@ -2,9 +2,12 @@ package com.favwest.coroutinestoyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,16 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.text_view)
         val vm = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         textView.text = vm.currentNumber.value.toString()
+
+        val numberObserver = Observer<Int> {
+            textView.text = it.toString()
+        }
+
+        vm.currentNumber.observe(this, numberObserver)
+
+        val countButton = findViewById<Button>(R.id.count_button)
+        countButton.setOnClickListener {
+            runBlocking { vm.countUpSlowly() }
+        }
     }
 }
--- app/src/main/java/com/favwest/coroutinestoyapp/MainActivityViewModel.kt
+++ app/src/main/java/com/favwest/coroutinestoyapp/MainActivityViewModel.kt
@@ -3,9 +3,17 @@ package com.favwest.coroutinestoyapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
 
-class MainActivityViewModel: ViewModel() {
-    private val _currentNumber = MutableLiveData<Int>(0)
+class MainActivityViewModel : ViewModel() {
+    private var _currentNumber = MutableLiveData<Int>(0)
     val currentNumber: LiveData<Int>
         get() = _currentNumber
+
+    suspend fun countUpSlowly() {
+        for (number in 0..100) {
+            delay(10L)
+            _currentNumber.value = number + 1
+        }
+    }
 }
